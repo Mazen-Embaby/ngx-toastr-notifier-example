8cadcc4d63b86595740b8dc6feb6921232fec5dd
Mazen-Embaby <eng.mazen.embaby@gmail.com>
2025-07-02 15:52:20 +0400

    add example

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,44 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-
+import { ToastService } from 'ngx-toastr-notifier';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
+  <h1> Hello from {{name}} </h1>
+  <p>
+    Start editing to see some magic happen :)
+  </p>
+  
+  <button (click)="showSuccess()">Success</button>
+  <button (click)="showError()">Error</button>
+  <button (click)="showInfo()">Info</button>
+  <button (click)="showWarning()">Warning</button>
   `,
 })
 export class App {
-  name = 'Angular';
+  name = 'Angular 20';
+  constructor(private toastr: ToastService){}
+
+  showSuccess(){
+   this.toastr.success('everything is broken', 'Major Error', {
+  duration: 3000,
+});
+  }
+  showError(){
+   this.toastr.error('everything is broken', 'Major Error', {
+    duration: 3000,
+});
+  }
+   showInfo(){
+   this.toastr.info('everything is broken', 'Major Error', {
+    duration: 3000,
+});
+  }
+   showWarning(){
+   this.toastr.warning('everything is broken', 'Major Error', {
+    duration: 3000,
+});
+  }
 }
 
 bootstrapApplication(App);
